948488d851bc8f52e5cf1db86aafdef822620cc2
Madhusudhan <dmadhusudhan98@gmail.com>
2024-07-08 22:27:56 +0530

    add

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create a transporter using SMTP
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'madhusudhan98@gmail.com',
+    subject: `Clinet Message${subject}`,
+    text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.post('/appointment-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'madhusudhan98@gmail.com', // Change this to the clinic's email address
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
